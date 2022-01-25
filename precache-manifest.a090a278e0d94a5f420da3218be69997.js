d120aaa23c2d88c8bcb4fad664ad50eaa5e2afba
sorbajit goswami <sorbajitgoswami@gmail.com>
2022-01-26 00:44:52 +0530

    Updates

---
+++ precache-manifest.a090a278e0d94a5f420da3218be69997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a76363a1e64921f7e89e67522e82aa2",
+    "url": "/Ultra-weather-app/index.html"
+  },
+  {
+    "revision": "06f3605aad26343b7c02",
+    "url": "/Ultra-weather-app/static/css/main.4b2dddbe.chunk.css"
+  },
+  {
+    "revision": "285eb76867bd3683bc7e",
+    "url": "/Ultra-weather-app/static/js/2.8462d6c3.chunk.js"
+  },
+  {
+    "revision": "83dd22b0688da01307759f0db1576406",
+    "url": "/Ultra-weather-app/static/js/2.8462d6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f3605aad26343b7c02",
+    "url": "/Ultra-weather-app/static/js/main.675a07cd.chunk.js"
+  },
+  {
+    "revision": "ff09a2d93129dd1cfa87",
+    "url": "/Ultra-weather-app/static/js/runtime-main.2e9fa02d.js"
+  }
+]);
